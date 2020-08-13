cd55ef7e6d5c0cd9cff10432f81e06038d386a1c
sakshi1499 <42520146+sakshi1499@users.noreply.github.com>
2020-08-13 19:49:54 +0530

    chore: Port docs from wiki to repo

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,11 +2,131 @@ module.exports = {
   docs: [
     {
       type: "doc",
-      id: "intro",
+      id: "home-page",
+    },
+    {
+      type: "link",
+      label: "AnitaB.org",
+      href: "https://anitab.org/",
+    },
+    {
+      type: "category",
+      label: "How To Contribute",
+      items: ['how-to-contribute', 'commit-message-style-guide', 'test-the-app', 'maintainer-guideline',],
+    },
+    {
+      type: "link",
+      label: "Project Requirements",
+      href: "https://docs.google.com/document/d/1LCGhXPDmwivQGG0EYeJaMlEzBsK9Tp-kOvX9aBTSdqY/edit",
+    },
+    {
+      type: "category",
+      label: "Setup & Install",
+      items: ['setup-osx', 'environment-variables',],
+    },
+    {
+      type: "link",
+      label: "User Interface Design",
+      href: "https://docs.google.com/document/d/1wNdZ_jNAuSNa_E6i9kXGY5i-eVMpCgDqm6R8-2w6bq0/edit?usp=sharing",
+    },
+    {
+      type: "category",
+      label: "Development",
+      items: ['tech-stack', 'coding-standard', 'code-org', 'user-auth', 'tech-decisions',],
     },
     {
       type: "doc",
-      id: "adding-pages",
+      id: "database-design",
+    },
+    {
+      type: "category",
+      label: "Main Concepts",
+      items: ['community-creation', 'community-resources', 'meetups-creation', 'groups-permissions', 'zoom-meetings',],
+    },
+    {
+      type:"doc",
+      id: "future-ideas",
+    },
+    {
+      type: "category",
+      label: "Contribution to the Docs",
+      items: ['intro', 'adding-pages',],
+    },
+    {
+      type: "doc",
+      id: "home-page",
+    },
+    {
+      type: "doc",
+      id: "how-to-contribute",
+    },
+    {
+      type: "doc",
+      id: "commit-message-style-guide",
+    },
+    {
+      type: "doc",
+      id: "test-the-app",
+    },
+    {
+      type: "doc",
+      id: "maintainer-guideline",
+    },
+    {
+      type: "doc",
+      id: "setup-osx",
+    },
+    {
+      type: "doc",
+      id: "environment-variables",
+    },
+    {
+      type: "doc",
+      id: "tech-stack",
+    },
+    {
+      type: "doc",
+      id: "coding-standard",
+    },
+    {
+      type: "doc",
+      id: "code-org",
+    },
+    {
+      type: "doc",
+      id: "user-auth",
+    },
+    {
+      type: "doc",
+      id: "tech-decisions",
+    },
+    {
+      type: "doc",
+      id: "database-design",
+    },
+    {
+      type:"doc",
+      id: "community-creation",
+    },
+    {
+      type:"doc",
+      id: "community-resources",
+    },
+    {
+      type:"doc",
+      id: "meetups-creation",
+    },
+    {
+      type:"doc",
+      id: "groups-permissions",
+    },
+    {
+      type:"doc",
+      id: "zoom-meetings",
+    },
+    {
+      type:"doc",
+      id: "future-ideas",
     },
   ],
 };
